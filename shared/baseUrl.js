d3f1cc07a54b3105ad3da879856e0c9b79557291
Martin Barrios <mar.barr1215@gmail.com>
2020-05-08 15:38:27 -0700

    Animatable

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,6 +1,6 @@
-export const baseUrl = 'http://192.168.1.103:3001/';
-//export const baseUrl = 'http://192.168.1.154:3001/';
-//export const baseUrl = 'http://10.0.1.9:3001/';
-
+//export const baseUrl = 'http://192.168.1.103:3001/';
+//export const baseUrl = 'http://192.168.1.154:3001/'; /* 2g */
+//export const baseUrl = 'http://10.0.1.9:3001/'; /* luis */
+export const baseUrl = 'http://192.168.1.162:3001/'; /* netgear */
 
 
