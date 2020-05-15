cc5d678e08aa827f45be9e013e97d63522b345c7
Martin Barrios <mar.barr1215@gmail.com>
2020-05-15 15:41:00 -0700

    Picking an Image

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Button, StyleSheet, Modal, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Button, StyleSheet, Modal, Alert, PanResponder, Share  } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -64,6 +64,15 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = ( title, message,url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url
+        }, {
+            dialogTitle: 'Share' + title
+        });
+    }
 
     if (campsite) {
         return (
@@ -97,6 +106,15 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            style={styles.cardItem}
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
